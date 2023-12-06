45b40fa16d41621b2bfc7f2cb2301c2a11ef233a
Srdjan <srdjan.s96@outlook.com>
2023-12-06 12:52:06 +0100

    Export Supabase API url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://irjxfxtcoefvjcxoiiws.supabase.co";
+
+export const supabaseUrl = "https://irjxfxtcoefvjcxoiiws.supabase.co";
+
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyanhmeHRjb2VmdmpjeG9paXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE2MDQwMDAsImV4cCI6MjAxNzE4MDAwMH0.Ih3GZvotxc90bwSQ6oIB69YSIiOduwAEALwAXmeZp2s";
 const supabase = createClient(supabaseUrl, supabaseKey);
